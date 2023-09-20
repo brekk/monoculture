120c62ae6803a67c6f3868d74be7eb490ce33a91
brekk <brekk@brekkbockrath.com>
2023-09-20 09:29:38 -0700

    closer

--- packages/monocle/src/index.spec.js
+++ packages/monocle/src/index.spec.js
@@ -1,16 +1,32 @@
 import path from 'node:path'
 import { fork } from 'fluture'
 import { pipe } from 'ramda'
-import { reader } from './reader'
+import { reader, monoprocessor } from './reader'
 
 test('reader', done => {
   const files = reader({ basePath: __dirname }, path.join(__dirname, '/*'))
-  // eslint-disable-next-line no-console
-  fork(console.warn)(
+  fork(e => {
+    // eslint-disable-next-line no-console
+    console.warn(e)
+    done()
+  })(
     pipe(z => {
       // eslint-disable-next-line no-console
       console.log('oh yeah', z)
       done()
     })
   )(files)
+})
+
+const PLUGINS = [
+  // {name: 'a', fn: (x) =>
+]
+
+test('monoprocessor', done => {
+  fork(done)(x => {
+    console.log('RAW', x)
+    done()
+  })(
+    monoprocessor({ basePath: __dirname }, PLUGINS, path.join(__dirname, '/*'))
+  )
 })
--- packages/monocle/src/reader.js
+++ packages/monocle/src/reader.js
@@ -1,6 +1,15 @@
 import path from 'node:path'
 
-import { curry, pipe, map, split, addIndex, chain, __ as $ } from 'ramda'
+import {
+  curry,
+  pipe,
+  map,
+  split,
+  addIndex,
+  fromPairs,
+  chain,
+  __ as $,
+} from 'ramda'
 import { parallel } from 'fluture'
 
 import { readDir, readFile } from 'file-system'
@@ -26,7 +35,22 @@ export const readMonoFile = curry((basePath, file) => {
 })
 
 export const reader = curry(({ basePath }, dirglob) =>
-  pipe(readDir, chain(pipe(map(readMonoFile(basePath)), parallel(10))))(dirglob)
+  pipe(
+    readDir,
+    chain(files =>
+      pipe(
+        map(readMonoFile(basePath)),
+        parallel(10),
+        map(content => ({
+          content,
+          files: pipe(
+            map(f => [f.hash, f.file]),
+            fromPairs
+          )(content),
+        }))
+      )(files)
+    )
+  )(dirglob)
 )
 
 export const monoprocessor = curry(({ basePath }, plugins, dirGlob) =>
