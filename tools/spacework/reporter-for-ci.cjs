448ad32a24693eb8feba6229a447c014ba1a9ef1
brekk <brekk@brekkbockrath.com>
2023-12-29 14:01:32 -0800

    try this!

--- tools/spacework/reporter-for-ci.cjs
+++ tools/spacework/reporter-for-ci.cjs
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 const {
+  any,
+  startsWith,
+  head,
   prepend,
   nth,
+  propOr,
+  slice,
+  trace,
   C,
   filter,
   fork,
@@ -10,29 +16,80 @@ const {
   map,
   pipe,
   readStdin,
+  tail,
+  reduce,
   split,
   trim,
   unlines,
+  j2,
 } = require('snang/script')
+const perc = z => z + '%'
+const summarizeNode = (project, s, b, f, l) =>
+  `**${project}**: ${printSummary(s, b, f, l)}`
+const printSummary = (s, b, f, l) =>
+  [s, b, f, l].map(z => `\`${z}\``).join(' / ')
 
 module.exports = pipe(
   readStdin,
   map(
     pipe(
-      lines,
-      filter(y => y[0] === ' ' && y[1] !== ' '),
-      map(split(C.p)),
-      map(map(trim)),
-      map(([n, ...rest]) => {
-        const y = split('/', n)
-        return [nth(1, y), ...rest]
-      }),
-      prepend(['\n_Legend_: Statements / Branches / Functions / Lines\n']),
-      map(([name, st, br, fn, ln]) =>
-        st
-          ? ` * **${name}**: \`${st}%\` / \`${br}%\` / \`${fn}%\` / \`${ln}%\``
-          : name
+      split(/\n\s{1}(\S+)/),
+      map(lines),
+      slice(1, Infinity),
+      reduce(
+        ({ active, stack }, [h, ...t]) => {
+          if (!active && !t.length) {
+            return { active: h, stack }
+          }
+          if (t.length) {
+            const summary = h
+              .trim()
+              .split('|')
+              .map(z => z.trim())
+              .filter(I)
+            const [statements, branches, functions, l] = summary
+            const files = t
+              .map(y =>
+                y
+                  .trim()
+                  .split('|')
+                  .map(z => z.trim())
+                  .slice(0, -1)
+              )
+              .filter(y => !any(startsWith('----'), y))
+              .map(([n, s, b, f, l2]) => ({
+                name: n,
+                s: perc(s),
+                b: perc(b),
+                f: perc(f),
+                l: perc(l2),
+              }))
+            stack.push({
+              project: active.split('/')[1],
+              s: perc(statements),
+              b: perc(branches),
+              f: perc(functions),
+              l: perc(l),
+              files,
+            })
+            active = null
+          } else {
+            active = h
+          }
+          return { active, stack }
+        },
+        { active: null, stack: [] }
       ),
+      propOr([], 'stack'),
+      slice(0, -1),
+      map(({ project, s, b, f, l, files }) => {
+        const sum = summarizeNode(project, s, b, f, l)
+        return ` * ${sum}\n\n     <details closed><summary>Files</summary>\n\n     * ${files
+          .map(({ name, s: s2, b: b2, f: f2, l: l2 }) =>
+            summarizeNode(name, s2, b2, f2, l2)
+          )
+          .join('\n     * ')}\n\n     </details>\n`
+      }),
       unlines
     )
   ),
