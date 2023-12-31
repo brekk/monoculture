905a152bd3f714f9fb31383f31cd2c10da61d1c6
brekk <brekk@brekkbockrath.com>
2023-12-30 16:00:30 -0800

    try again!

--- tools/spacework/reporter-for-ci.cjs
+++ tools/spacework/reporter-for-ci.cjs
@@ -51,63 +51,64 @@ const YARGS_CONFIG = {
 function reporter(args) {
   return pipe(
     raw => yargsParser(raw, YARGS_CONFIG),
-    ({ _: parsedArguments, pr, branch }) =>
-      pipe(
-        split(/\n\s{1}(\S+)/),
-        map(lines),
-        slice(1, Infinity),
-        reduce(
-          ({ active, stack }, [h, ...t]) => {
-            if (!active && !t.length) {
-              return { active: h, stack }
-            }
-            if (t.length) {
-              const summary = h
-                .trim()
-                .split('|')
-                .map(z => z.trim())
-                .filter(I)
-              const [statements, branches, functions, l] = summary
-              const files = t
-                .map(y =>
-                  y
-                    .trim()
-                    .split('|')
-                    .map(z => z.trim())
-                    .slice(0, -1)
-                )
-                .filter(y => !any(startsWith('----'), y))
-                .map(([n, s, b, f, l2]) => ({
-                  name: n,
-                  s: perc(s),
-                  b: perc(b),
-                  f: perc(f),
-                  l: perc(l2),
-                }))
-              const pathParts = active.split('/')
-              stack.push({
-                path: pathParts.slice(0, 2).join('/'),
-                project: pathParts[1],
-                s: perc(statements),
-                b: perc(branches),
-                f: perc(functions),
-                l: perc(l),
-                files,
-              })
-              active = null
-            } else {
-              active = h
-            }
-            return { active, stack }
-          },
-          { active: null, stack: [] }
-        ),
-        propOr([], 'stack'),
-        slice(0, -1),
-        map(
-          ({ project, s, b, f, l, path, files }) =>
-            // eslint-disable-next-line max-len
-            `\n * <details closed><summary><strong><a href="${REPO}/${branch}/${path}">${project}</a></strong> (<code>${s}</code> / <code>${b}</code> / <code>${f}</code> / <code>${l}</code>)</summary>
+    ({ pr, branch }) =>
+      map(
+        pipe(
+          split(/\n\s{1}(\S+)/),
+          map(lines),
+          slice(1, Infinity),
+          reduce(
+            ({ active, stack }, [h, ...t]) => {
+              if (!active && !t.length) {
+                return { active: h, stack }
+              }
+              if (t.length) {
+                const summary = h
+                  .trim()
+                  .split('|')
+                  .map(z => z.trim())
+                  .filter(I)
+                const [statements, branches, functions, l] = summary
+                const files = t
+                  .map(y =>
+                    y
+                      .trim()
+                      .split('|')
+                      .map(z => z.trim())
+                      .slice(0, -1)
+                  )
+                  .filter(y => !any(startsWith('----'), y))
+                  .map(([n, s, b, f, l2]) => ({
+                    name: n,
+                    s: perc(s),
+                    b: perc(b),
+                    f: perc(f),
+                    l: perc(l2),
+                  }))
+                const pathParts = active.split('/')
+                stack.push({
+                  path: pathParts.slice(0, 2).join('/'),
+                  project: pathParts[1],
+                  s: perc(statements),
+                  b: perc(branches),
+                  f: perc(functions),
+                  l: perc(l),
+                  files,
+                })
+                active = null
+              } else {
+                active = h
+              }
+              return { active, stack }
+            },
+            { active: null, stack: [] }
+          ),
+          propOr([], 'stack'),
+          slice(0, -1),
+          map(
+            ({ project, s, b, f, l, path, files }) =>
+              // eslint-disable-next-line max-len
+              `\n * <details closed><summary><strong><a href="${REPO}/${branch}/${path}">${project}</a></strong> (<code>${s}</code> / <code>${b}</code> / <code>${f}</code> / <code>${l}</code>)</summary>
 
    - ${files
      .map(({ name, s: s2, b: b2, f: f2, l: l2 }) =>
@@ -116,12 +117,13 @@ function reporter(args) {
      .join('\n   - ')}
 
    </details>`
-        ),
-        unlines
-      )(parsedArguments.join('\n')),
-    console.log
+          ),
+          unlines
+        )
+      )(readStdin()),
+    // map(console.log)
     // eslint-disable-next-line no-console
-    // fork(console.error, console.log)
+    fork(console.error, console.log)
   )(args)
 }
 const ARGS = process.argv.slice(2)
