6bd0a5c28185f23a37123a3989c86141bcc884f0
brekk <brekk@brekkbockrath.com>
2024-02-05 20:00:38 -0800

    :shrug: this is weaksauce

--- typed-packages/microplastic/src/index.spec.ts
+++ typed-packages/microplastic/src/index.spec.ts
@@ -7,15 +7,15 @@ const productIdSelector = (z: string): string => {
   return d > -1 ? z.slice(0, d) : 'NO_MATCH'
 }
 
-test('remeda -- equalishBy', () => {
-  const isCoolProduct = equalishBy<string, string>(productIdSelector, 'coolco')
-  // THIS WORKS, BUT IS TYPICAL TS SLOP
-  // const isApplicablyCool = (y: string) => equalishBy(productIdSelector, y)
-  const isApplicablyCool = equalishBy(productIdSelector)
-  // TS is sad:
-  const isCoolProduct2 = isApplicablyCool('coolco')
+test('smoke test -- productIdSelector', () => {
   expect(productIdSelector('coolco-292929')).toEqual('coolco')
   expect(productIdSelector('2020202')).toEqual('NO_MATCH')
+})
+test('remeda -- equalishBy - no curry', () => {
+  const isCoolProduct = equalishBy<string, string>(productIdSelector, 'coolco')
+  // is this the best we can do today?
+  const isApplicablyCool = (y: string) => equalishBy(productIdSelector, y)
+  const isCoolProduct2 = isApplicablyCool('coolco')
 
   expect(isCoolProduct('coolco-292929')).toBeTruthy()
   expect(isCoolProduct('2020202')).toBeFalsy()
@@ -25,16 +25,47 @@ test('remeda -- equalishBy', () => {
 
   // this correctly yells about incorrect types
   // productIdSelector(/2/g)
+
+  expect(equalishBy((a: string): string => a, 'coolco-101')).toBeTruthy()
 })
-/* eslint-disable no-console */
-test('ramda -- equalishBy', () => {
-  console.log(rBy, typeof rBy)
-  const selecto: EqualishBy<string, string> = rBy<string, string>(
-    productIdSelector
-  )
-  console.log('selecto', selecto)
+
+test('remeda -- equalishBy - inline-type', () => {
+  const isCoolProduct = equalishBy<string, string>(productIdSelector, 'coolco')
+  // this doesn't work
+  // const isApplicablyCool: EqualishBy<string, string> =
+  //   equalishBy(productIdSelector)
+  type FunType = (y: string) => EqualishBy<string, string>
+  const isApplicablyCool: FunType = equalishBy(productIdSelector)
+  const isCoolProduct2 = isApplicablyCool('coolco')
+
+  expect(isCoolProduct('coolco-292929')).toBeTruthy()
+  expect(isCoolProduct('2020202')).toBeFalsy()
+
+  expect(isCoolProduct2('coolco-292929')).toBeTruthy()
+  expect(isCoolProduct2('2020202')).toBeFalsy()
+
+  expect(equalishBy((a: string): string => a, 'coolco-101')).toBeTruthy()
+})
+
+test('remeda -- equalishBy - curry', () => {
+  const isCoolProduct = equalishBy<string, string>(productIdSelector, 'coolco')
+  const isApplicablyCool = equalishBy(productIdSelector)
+  // this fails TS
+  const isCoolProduct2 = isApplicablyCool('coolco')
+
+  expect(isCoolProduct('coolco-292929')).toBeTruthy()
+  expect(isCoolProduct('2020202')).toBeFalsy()
+  expect(isCoolProduct2('coolco-292929')).toBeTruthy()
+  expect(isCoolProduct2('2020202')).toBeFalsy()
+
+  // TS hates this
+  // const yes = equalishBy((a: string): string => a)
+  // expect(yes('coolco-101')).toBeTruthy()
+  expect(equalishBy((a: string): string => a, 'coolco-101')).toBeTruthy()
+})
+test('ramda  -- equalishBy', () => {
+  const selecto = rBy<string, string>(productIdSelector)
   const isCoolProduct = selecto('coolco')
-  console.log('isCoolProduct', isCoolProduct)
   expect(isCoolProduct('coolco-292929')).toBeTruthy()
   expect(isCoolProduct('2020202')).toBeFalsy()
   const isNum = rBy<any, string>(x => typeof x, 'number')
@@ -42,4 +73,14 @@ test('ramda -- equalishBy', () => {
   expect(isNum('barf')).toBeFalsy()
   // currently doesn't barf without overloads
   // const hey = rBy<string, string>('test')
+})
+
+test('ramda  -- equalishBy - undeclared application', () => {
+  const selecto: EqualishBy<string, string> = rBy(productIdSelector)
+  const isCoolProduct = selecto('coolco')
+  expect(isCoolProduct('coolco-292929')).toBeTruthy()
+  expect(isCoolProduct('2020202')).toBeFalsy()
+  const isNum: EqualishBy<any, string> = rBy(x => typeof x, 'number')
+  expect(isNum(20000)).toBeTruthy()
+  expect(isNum('barf')).toBeFalsy()
 })
--- typed-packages/microplastic/src/index.ts
+++ typed-packages/microplastic/src/index.ts
@@ -5,9 +5,22 @@ interface Transformer<F, T> {
   (arg: F): T
 }
 
-export const $equalishBy = (x, transform, expected) => {
+/*
+this higher-specificy TS version does nothing I can see
+export function $equalishBy<F, T>(
+  x: F,
+  transform: Transformer<F, T>,
+  expected: T
+): boolean {
   // eslint-disable-next-line no-console
   console.log({ x, transform, expected })
+  const t = transform(x)
+  return t === expected
+}
+*/
+
+export const $equalishBy = (x, transform, expected) => {
+  // eslint-disable-next-line no-console
   const t = transform(x)
   return t === expected
 }
